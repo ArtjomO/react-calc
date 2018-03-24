973d878ea9b5414702126d48858d5ffb5f60adf3
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2018-03-24 04:44:46 +0200

    date is styled, month are switching backwards and forward

--- src/app/Components/Calendar.jsx
+++ src/app/Components/Calendar.jsx
@@ -16,19 +16,23 @@ export class Calendar extends React.Component {
 
     days = ['Sunday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Monday']
 
-    
-   
-    
+    switchMonth(plusMinus){
+        this.setState({
+            monthCount: this.state.monthCount + plusMinus
+        })
+    }
     
     render() {
         return (
             <div className="container">
-                <div className="row">
-                    <span>{generateMonth(this.state.monthCount).dateToRender} </span>
+                <div className="cal-date row justify-content-center">
+                    <div className="arrow arrow-left" onClick={() => this.switchMonth(-1)}></div>
+                    <div className="date">{generateMonth(this.state.monthCount).dateToRender} </div>
+                    <div className="arrow arrow-right" onClick={() => this.switchMonth(1)}></div>
                 </div>
 
                 <div className="cal-head row justify-content-center">
-                    {this.days.map(day => <div className="cal-dayOfweek" key={day}> {day} </div>)}
+                    {this.days.map(day => <div className="cal-dayOfweek" key={shortid.generate()}> {day} </div>)}
                 </div>
 
                 <div className="cal-body row justify-content-center">
