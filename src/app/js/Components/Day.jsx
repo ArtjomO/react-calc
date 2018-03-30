69b631d9b2daae1aa8a2345a0a30df2b45f535cd
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2018-03-30 08:45:28 +0300

    popup doesnt work, searching alrernative

--- src/app/js/Components/Day.jsx
+++ src/app/js/Components/Day.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
+const smStyle = {
+    position: "fixed"
+}
+
 export class Day extends React.Component {
-
+    constructor(){
+        super();
+        this.state = {
+            hover: true
+        }
+    }
     
-    render(){
+    toggleState(){
+        this.setState({
+            hover: !this.state.hover
+        })
+        console.log(this.state.hover)
+    }
 
+    render(){
         return (
-            <div className="day container">
+            <div className="day container" style={smStyle} onMouseEnter={this.toggleState.bind(this)} onMouseLeave={this.toggleState.bind(this)}>
                 <div className="date row  justify-content-end">
                     <span> {this.props.date} </span>
                 </div>
