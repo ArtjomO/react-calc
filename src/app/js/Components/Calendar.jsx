f7284509ff35e8e4c2f98bb66b67d26457416b0b
Artjoms Ostapenko <artjom.ostapenko@gmail.com>
2018-04-23 06:44:22 +0300

    all bootstrap classes are deleted from caledar, trying to resolve position and z-index problems when hovering element

--- src/app/js/Components/Calendar.jsx
+++ src/app/js/Components/Calendar.jsx
@@ -24,18 +24,18 @@ export class Calendar extends React.Component {
     
     render() {
         return (
-            <div className="container">
-                <div className="cal-date row justify-content-center">
+            <div className="cal">
+                <div className="cal-date">
                     <div className="arrow arrow-left" onClick={() => this.switchMonth(-1)}></div>
                     <div className="date">{generateMonth(this.state.monthCount).dateToRender} </div>
                     <div className="arrow arrow-right" onClick={() => this.switchMonth(1)}></div>
                 </div>
 
-                <div className="cal-head row justify-content-center">
+                <div className="cal-head">
                     {this.days.map(day => <div className="cal-dayOfweek" key={shortid.generate()}> {day} </div>)}
                 </div>
 
-                <div className="cal-body row justify-content-center">
+                <div className="cal-body">
                     { generateMonth(this.state.monthCount).fullMonth.map(date =>  <Day key={shortid.generate()} date={date}/>) }
                 </div>
             </div>
--- src/app/js/Components/Day.jsx
+++ src/app/js/Components/Day.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-const smStyle = {
-    position: "fixed"
-}
 
 export class Day extends React.Component {
     constructor(){
@@ -21,13 +18,13 @@ export class Day extends React.Component {
 
     render(){
         return (
-            <div className="day container" style={smStyle} onMouseEnter={this.toggleState.bind(this)} onMouseLeave={this.toggleState.bind(this)}>
-                <div className="date row  justify-content-end">
+            <div className="day"  onMouseEnter={this.toggleState.bind(this)} onMouseLeave={this.toggleState.bind(this)}>
+                <div className="date">
                     <span> {this.props.date} </span>
                 </div>
-                <div className="shiftType row"></div>
-                <div className="shiftType row"></div>
-                <div className="shiftType row"></div>
+                <div className="shiftType"></div>
+                <div className="shiftType"></div>
+                <div className="shiftType"></div>
             </div>
         )
     }
